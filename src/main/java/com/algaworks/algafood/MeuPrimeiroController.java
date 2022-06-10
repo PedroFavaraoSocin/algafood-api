61c8df5a567205c8d35d3871abbd6189bae454c9
Pedro <pedro.favarao@socin.com.br>
2022-06-10 09:39:25 -0300

    Commit:

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -19,7 +19,7 @@ public class MeuPrimeiroController {
 		System.out.println("MeuPrimeiroController: " + ativacaoClienteService);
 	}
 
-	@GetMapping("/hellooo")
+	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
 		Cliente joao = new Cliente("João", "joão@xyz.com.br", "8998675849");
